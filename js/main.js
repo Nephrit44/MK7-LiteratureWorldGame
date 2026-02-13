7dd7d0fea360110e24b1ff724e404ca7fc3a9bdb
Nephrit44 <nephrit44@yandex.ru>
2026-02-13 17:41:05 +0300

    Добавлена база с вопросами

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const header = document.querySelector('.header');
 const header__button = document.querySelector('.header__button');
 const questions = document.querySelector('.questions');
 const questions__itemsLVL1 = document.querySelector('.questions__items-lvl1');
+const questions__itemsLVL2 = document.querySelector('.questions__items-lvl2');
 
 header__button.addEventListener('click', function () {
     header.classList.toggle('visible');
@@ -9,16 +10,16 @@ header__button.addEventListener('click', function () {
 });
 
 //Загрузка списка вопросов № 1
-loadQuestions(arrColletion);
+loadQuestions(arrColletion_QLV1, questions__itemsLVL1);
+loadQuestions(arrColletion_QLV2, questions__itemsLVL2);
 
-
-function loadQuestions(arrColletion){
+function loadQuestions(arrColletion, htmlElement){
     arrColletion.forEach(element => {
         let liElemenet = document.createElement('li');
         liElemenet.setAttribute('qID', element.questions);
         liElemenet.innerHTML = element.questions;
         liElemenet.classList.add ('questions__item');
-        questions__itemsLVL1.appendChild(liElemenet);
+        htmlElement.appendChild(liElemenet);
     });
 }
 
